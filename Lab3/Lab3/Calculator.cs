5809d8ff2f645f00c390d4db93d1888a94a7e683
dRrankoi <vladislav15679@gmail.com>
2021-09-28 20:32:24 +0300

    Proc

---
+++ Lab3/Lab3/Calculator.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab3
+{
+    class Calculator
+    {
+        double x;
+        double y;
+
+        public Calculator(double X, double Y, char op)
+        {
+            x = X;
+            y = Y;
+
+            if (op == '+') Addition();
+            else if (op == '-') Substraction();
+            else if (op == '*') Multiplication();
+        }
+
+        public void Addition()
+        {
+            Console.WriteLine($"Ответ: {x + y}");
+        }
+        public void Substraction()
+        {
+            Console.WriteLine($"Ответ: {x - y}");
+        }
+
+        public void Multiplication()
+        {
+            Console.WriteLine($"Ответ: {x * y}");
+        }
+
+       
+        
+    }
+}
---
+++ Lab3/Lab3/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+
+using System.Linq;
+
+namespace Lab3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] numbers = new char[] { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+            bool proc = true;
+
+            try
+            {
+                switch (proc)
+                {
+                    case true:
+                        {
+                            Console.WriteLine("Введите пример с данными операциями ( + | - | / )");
+                            string readline = Console.ReadLine();
+
+                            double x = Double.Parse(string.Concat(readline.TakeWhile(char.IsDigit)));
+
+                            char op = readline.First(x => x.Equals('*') || x.Equals('+') || x.Equals('-'));
+
+                            int IndexOp = readline.IndexOf(op);
+
+                            string StringAfterOp = readline.Substring(IndexOp + 1);
+
+                            int IndexDigitsAfterOp = StringAfterOp.IndexOfAny(numbers);
+
+                            string StringDigitsAfterOp = StringAfterOp.Substring(IndexDigitsAfterOp);
+
+                            double y = Double.Parse(string.Concat(StringDigitsAfterOp.TakeWhile(char.IsDigit)));
+
+                            Calculator calculator = new Calculator(x, y, op);
+
+                            goto case true;
+                        }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+
+ }
+
